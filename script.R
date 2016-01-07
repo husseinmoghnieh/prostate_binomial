1f4d305b4d430d6106757f5df5dec50e5ee18ea4
huss <hussein.moghnieh@gmail.com>
2016-01-06 23:15:59 -0500

    modify readme export prediction after creating tmp directory

--- script.R
+++ script.R
@@ -7,7 +7,7 @@ h2o_df$CAPSULE = as.factor(h2o_df$CAPSULE)
 rand_vec <- h2o.runif(h2o_df, seed = 1234)
 
 train <- h2o_df[rand_vec <= 0.8,]
-valid <- h2o_df[(rand_vec > 0.8) & (rand_vec <= 0.9),]
+valid <- h2o_df[(rand_vec > 0.8),]
 binomial.fit = h2o.glm(y = "CAPSULE", x = c("AGE", "RACE", "PSA", "GLEASON"), training_frame = train, validation_frame = valid, family ="binomial", model_id = "ProstateModel")
 
 # Make and export predictions.
@@ -17,12 +17,12 @@ h2o_single_test_2  <- h2o.importFile(path = "data/prostate_test_actual1.csv")
 pred_test_1 = h2o.predict(binomial.fit, h2o_single_test_1)
 pred_test_2 = h2o.predict(binomial.fit, h2o_single_test_2)
 
-h2o.exportFile(pred_test_1, "tmp/pred_test_1.csv", force = TRUE)
-h2o.exportFile(pred_test_2, "tmp/pred_test_2.csv", force = TRUE)
-
 if (! file.exists("tmp")) {
   dir.create("tmp")
 }
+
+h2o.exportFile(pred_test_1, "tmp/pred_test_1.csv", force = TRUE)
+h2o.exportFile(pred_test_2, "tmp/pred_test_2.csv", force = TRUE)
 h2o.download_pojo(binomial.fit , path = "tmp")
 
 # Or you can export the predictions to hdfs:
